7c859e6c1adf3ff4aadf9a0e7ec4531ff0d492d5
Cristian Ramirez F <cristianramirezdev@hotmail.com>
2021-07-09 14:55:43 -0500

    First commit

---
+++ tests/features/steps/hooks.js
@@ -0,0 +1,18 @@
+const { Builder } = require('selenium-webdriver')
+const { Before, After } = require('@cucumber/cucumber');
+const chrome = require('selenium-webdriver/chrome');
+require('chromedriver');
+
+
+Before(async () => {
+    global.driver = await new Builder().forBrowser('chrome')
+      .setChromeOptions(new chrome.Options().headless())
+      .build()
+
+    await driver.get("http://demo.guru99.com/test/newtours/")
+    await driver.manage().window().setRect(1440, 1080)
+});
+
+After(async () => {
+    await driver.quit();
+});
